8b92501b28d03e2f80ef3d9012283efed92cc397
Bilal Haider <haiderbilal306@gmail.com>
2024-01-25 23:17:02 +0500

    Add Cart List

---
+++ Cart.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+#include "Product.h"
+#include "Node.h"
+class Cart
+{
+    private:
+    Node *head;
+    Node *tail;
+    int size;
+
+public:
+    Cart()
+    {
+        size = 0;
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    int getSize() const
+    {
+        return size;
+    }
+    Node* getCart()
+    {
+        return this->head;
+    }
+
+    void Add_to_Cart(Product p)
+    {
+        Node *N = new Node(p);
+        if (head==NULL)
+        {
+            N->setNextPtr(nullptr);
+            head = N;
+            tail = N;
+            size++;
+            return;
+        }
+
+        tail->setNextPtr(N);
+        tail = tail->getNextPtr();
+        size++;
+        return;
+    }
+
+    bool deleteProduct(string ID)
+    {
+        if (size == 0)
+            return false;
+        if (ID == head->getData().getProduct_ID())
+        {
+            Node *current = head;
+            head = nullptr;
+            tail = nullptr;
+            delete current;
+            size--;
+            return true;
+        }
+
+        Node *current = head;
+        Node *previous;
+        while (current->getNextPtr() != NULL)
+        {
+            previous = current;
+            current = current->getNextPtr();
+            if (ID == (current->getData().getProduct_ID()))
+            {
+                previous->setNextPtr(current->getNextPtr());
+                size--;
+                delete current;
+                return true;
+            }
+        }
+        return false;
+    }
+    void print_cart()
+    {
+        Node *curr = head;
+        cout << "-----------------------------------------\n| "
+             << category << "\t\t\t\t|\n-----------------------------------------\n";
+        cout << "Product ID \tName\t\tPrice" << endl;
+        while (curr != nullptr)
+        {
+            cout << curr->getData().getProduct_ID() << "\t\t" << curr->getData().getProduct_name() << "\t\tRs." << curr->getData().getProduct_price() << endl;
+            curr = curr->getNextPtr();
+        }
+        cout << "\n\n\n";
+    }
+
+};
