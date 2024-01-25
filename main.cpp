7dd2e9d767f3ab9bd23ab29a5807bc20e9c168d1
AbdulSaboor-git <abdsab.pk@gmail.com>
2024-01-25 22:01:08 +0500

    update

--- main.cpp
+++ main.cpp
@@ -1,24 +1,56 @@
 #include <iostream>
-using namespace std;
 #include "Menu.cpp"
-void Menu()
-{
-    cout << "1.See Menu\n2.Add New Item in Menu\n3.Delete Item in Menu\n4.Exit Menu setting" << endl;
-    //Menu menu;
+using namespace std;
 
+void menuManager()
+{
+    Menu menu;
+    while (true)
+    {
+        system("cls");
+        cout << "\t\tFri-Chicks\n";
+        cout << "............ Menu Management ...........\n\n";
+
+        cout << "1. See Menu \n2. Add New Item in Menu \n3. Delete Item in Menu \n4. Go Back" << endl;
+        cout << "\nEnter the corresponding number ";
+        char input;
+        cin >> input;
+
+        switch (input)
+        {
+        case '1':
+            menu.Display_menu();
+            break;
+        case '2':
+            menu.AddItem();
+            break;
+        case '3':
+            menu.deleteItem();
+            break;
+        case '4':
+            return;
+        default:
+            break;
+        }
+    }
 }
+
 int main()
 {
-    cout << "\t\tFri-Chicks\n" << endl;
-    cout << "1.Menu Setting\n2.Generate Bill\n3.Billing History\n4.Total Sales\n5.Exit" << endl;
-    cout << "Enter the corresponding number ";
-    char input;
-    cin >> input;
-    switch(input)
+    while (true)
     {
+        system("cls");
+        cout << "\t\tFri-Chicks\n\n";
+        cout << "1. Manage Menu \n2. Generate Bill \n3. Billing History \n4. Total Sales \n5. Exit" << endl;
+        cout << "\nEnter the corresponding number ";
+
+        char input;
+
+        cin >> input;
+        switch (input)
+        {
         case '1':
-            //clear screen statment
-            Menu();
+            menuManager();
             break;
         case '2':
             break;
@@ -27,9 +59,10 @@ int main()
         case '4':
             break;
         case '5':
-            break;
+            return 0;
         default:
-            return 0; //will change it to show input again if invalid
+            break;
+        }
     }
     return 0;
 }
