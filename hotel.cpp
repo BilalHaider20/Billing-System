b3105eb67dc22a06f3480111dc6107aff94d44ca
Bilal Haider <haiderbilal306@gmail.com>
2024-01-26 20:58:32 +0500

    there is error in swicth of take order function

--- hotel.cpp
+++ hotel.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
-#include "Menu.cpp"
 #include <stack>
 #include <string>
+#include <conio.h>
+
+#include "Menu.cpp"
 #include "order.h"
+#include "productsList.h"
+
 using namespace std;
+
 class Hotel
 {
 private:
     string name;
-    stack<Order> stack;
-    int invoiceN = 10000000;
+    stack<Order> orderStack;
+    int invoiceNumber = 10000000;
     Menu menu;
-    Categories *categoriesList = menu.get_CategoriesList();
 
 public:
     Hotel()
     {
         name = "-";
     }
+
     Hotel(string name)
     {
         this->name = name;
@@ -26,84 +31,117 @@ public:
     int GenerateBill(ProductsList *cart)
     {
         Node<Product> *temp = cart->getHead();
-
         double bill = 0;
 
         while (temp != nullptr)
         {
             bill += temp->getData().getProduct_price();
-
             temp = temp->getNextPtr();
         }
 
         return bill;
     }
 
-    void Billing_History()
+    void BillingHistory()
     {
-        while (!stack.empty())
+        while (!orderStack.empty())
         {
-            Order obj = stack.top();
-            cout << "Costumer Name : " << obj.getCustomerName() << endl;
-            cout << "Invoice Number : " << obj.getInvoiceNumber() << endl;
-            cout << "Items Purchased  \n";
+            Order obj = orderStack.top();
+            cout << "Customer Name: " << obj.getCustomerName() << endl;
+            cout << "Invoice Number: " << obj.getInvoiceNumber() << endl;
+            cout << "Items Purchased:\n";
             ProductsList *products = obj.GetPurchasedItems();
             products->print();
             cout << "----------------------------------------------" << endl;
-            cout << "Total Bill\t\t" << obj.getBill();
+            cout << "Total Bill\t\t" << obj.getBill() << endl;
+            orderStack.pop();
         }
     }
 
-    void Take_order()
+    void TakeOrder()
     {
         if (menu.get_CategoriesList()->getSize() == 0)
         {
-            cout << "Menu is Empty ! \nPlease add Categories and items in menu " << endl;
+            cout << "Menu is Empty!\nPlease add Categories and items in the menu." << endl;
             pressToContinue();
             return;
         }
-        ProductsList *cart;
-        ProductsList *temp;
+
+        ProductsList *cart = new ProductsList();
+        ProductsList *temp=NULL;
+
         menu.Display_menu();
+
         cout << "------------------------------" << endl;
-        cout << "\nCostumer Name ";
+        cout << "\nCustomer Name: ";
         cin.ignore();
-        cin.clear();
         string name;
-        getline(cin,name);
+        getline(cin, name);
+
+        cout << name << ", what do you want to order? " << endl;
         while (true)
         {
-            cout << name<<" What do you want to order ? " << endl;
             cout << "\nSelect Category: \n";
-            Node<ProductsList *> *curr = categoriesList->getHead();
 
-            for (int i = 1; i <= categoriesList->getSize(); i++)
+            Node<ProductsList *> *curr = menu.get_CategoriesList()->getHead();
+
+            for (int i = 1; i <= menu.get_CategoriesList()->getSize(); i++)
             {
                 cout << i << ". " << curr->getData()->getCategory() << endl;
                 curr = curr->getNextPtr();
             }
+
             int c;
             cin >> c;
-            if(c>menu.get_CategoriesList()->getSize() || c<menu.get_CategoriesList()->getSize())
-            continue;
-            temp = categoriesList->get_Category(c);
+
+            if (c > menu.get_CategoriesList()->getSize() || c < 1)
+                continue;
+
+            temp = menu.get_CategoriesList()->get_Category(c);
             break;
         }
-        system("CLS");
-        temp->print();
-        cout << "--------------------------------------------" << endl;
-        // product selection code will be here
 
+        while (true)
+        {
+            system("CLS");
+            temp->print();
+            cout << "--------------------------------------------" << endl;
+            cout << "Enter the serial number to add to Cart: ";
+            int input;
+            cin >> input;
+            if (input > temp->getSize() || input < 1)
+            {
+                cout << "Invalid input!\nPlease enter a correct serial number" << endl;
+                pressToContinue();
+                continue;
+            }
+            
+            Product p = temp->getProduct(input);
+            cart->addProduct(p);
+
+            cout << "\nWant to select another product? (y/n) ";
+            char c;
+            cin >> c;
+            switch (c)
+            {
+            case 'y':
+            case 'Y':
+                continue;
+            case 'n':
+            case 'N':
+            double bill = GenerateBill(cart);
+            Order newOrder(name, invoiceNumber++, bill, cart);
+            orderStack.push(newOrder);
+            delete cart;
+                break;
+            default:
+                break;
+            }
+        }
         
-
-        // cart->addProduct(product to be added in cart);
-        double bill = GenerateBill(cart);
-
-        Order new_order(name, invoiceN++, bill, cart);
-        stack.push(new_order);
     }
 
-    void menuManager()
+    void MenuManager()
     {
         while (true)
         {
@@ -112,7 +150,7 @@ public:
             cout << "............ Menu Management ...........\n\n";
 
             cout << "1. See Menu \n2. Add Category \n3. Delete Category \n4. Add New Items in Menu \n5. Delete Items from Menu \n6. Go Back" << endl;
-            cout << "\nEnter the corresponding number ";
+            cout << "\nEnter the corresponding number: ";
             char input;
             cin >> input;
 
@@ -151,22 +189,25 @@ public:
             cout << "........................................\n\n";
 
             cout << "1. Manage Menu \n2. Take Orders \n3. Billing History \n4. Total Sales \n5. Exit" << endl;
-            cout << "\nEnter the corresponding number ";
+            cout << "\nEnter the corresponding number: ";
 
             char input;
-
             cin >> input;
+
             switch (input)
             {
             case '1':
-                menuManager();
+                MenuManager();
                 break;
             case '2':
-                Take_order();
+                TakeOrder();
                 break;
             case '3':
+                BillingHistory();
+                pressToContinue();
                 break;
             case '4':
+
                 break;
             case '5':
                 return;
@@ -175,9 +216,10 @@ public:
             }
         }
     }
+
     void pressToContinue()
     {
-        cout << "\n\nPress to continue...\n\n";
+        cout << "\n\nPress any key to continue...\n\n";
         getch();
     }
 };
